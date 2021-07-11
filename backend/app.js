a34566e1c7513de6d9ec95b624558afa175ee477
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-07-11 09:45:24 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,7 @@ app.get("/crash-test", () => {
 });
 
 app.post(
-  "/signin",
+  "api/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
@@ -73,7 +73,7 @@ app.post(
 );
 
 app.post(
-  "/signup",
+  "api/signup",
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
